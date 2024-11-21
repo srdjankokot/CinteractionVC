704b5a7bd13e36ab9fa030371a076c82aaacb4c4
Radovan92 <kljestan.radovan@gmail.com>
2024-11-21 12:03:46 +0100

    Test outlook auth 1

--- web/outlook-addin/src/taskpane/taskpane.js
+++ web/outlook-addin/src/taskpane/taskpane.js
@@ -1,28 +1,47 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global document, Office */
-
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    const appBody = document.getElementById("app-body");
+    if (appBody) {
+      appBody.style.display = "flex";
+      appBody.style.flexDirection = "column"; // Set flex-direction to column
+    }
+
+    document.getElementById("helloButton").onclick = run;
   }
 });
 
 export async function run() {
-  /**
-   * Insert your Outlook code here
-   */
+  const authUrl = "https://cinteraction.nswebdevelopment.com/web/auth";
 
-  const item = Office.context.mailbox.item;
-  let insertAt = document.getElementById("item-subject");
-  let label = document.createElement("b").appendChild(document.createTextNode("Subject: "));
-  insertAt.appendChild(label);
-  insertAt.appendChild(document.createElement("br"));
-  insertAt.appendChild(document.createTextNode(item.subject));
-  insertAt.appendChild(document.createElement("br"));
+  Office.context.ui.displayDialogAsync(
+    authUrl,
+    { height: 50, width: 50, displayInIframe: true },
+    function (asyncResult) {
+      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+        console.error("Dialog failed to open:", asyncResult.error.message);
+        return;
+      }
+
+      const dialog = asyncResult.value;
+      window.addEventListener("message", (event) => {
+        if (event.origin !== "expected_origin") {
+          console.error("Message received from unknown source:", event.origin);
+          return;
+        }
+
+        console.log("Message received from Flutter:", event.data);
+        const message = event.data;
+        if (message.startsWith("success:")) {
+          const accessToken = message.split(":")[1];
+          console.log("Access Token:", accessToken);
+        } else {
+          console.error("Login failed or user canceled");
+        }
+      });
+
+      dialog.addEventHandler(Office.EventType.DialogEventReceived, () => {
+        console.log("Dialog closed by user or system");
+      });
+    }
+  );
 }
