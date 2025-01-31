a2b2df7333f3dbee6e233d1cf9310c65b20cfdd5
srdjankokot <kokotsrdjan@gmail.com>
2025-01-31 12:42:00 +0100

    change turn server

--- lib/core/util/conf.dart
+++ lib/core/util/conf.dart
@@ -5,23 +5,23 @@ import 'package:janus_client/janus_client.dart';
 // bool withCredentials = false;
 // String apiSecret = "";
 //
-// String mixTurnServerUsername = "nswd";
-// String mixTurnServerCredential = "vcnswd321";
-//
-// int maxPublishersDefault = 9;
-//
-// List<RTCIceServer> iceServers = <RTCIceServer>[
-//
-//   RTCIceServer(urls: "stun:vc.cinteraction.com:3478", username: "", credential: ""),
-//   RTCIceServer(
-//       urls: "turn:vc.cinteraction.com:3478?transport=udp",
-//       username: mixTurnServerUsername,
-//       credential: mixTurnServerCredential),
-//   RTCIceServer(
-//       urls: "turn:vc.cinteraction.com:3478?transport=tcp",
-//       username: mixTurnServerUsername,
-//       credential: mixTurnServerCredential),
-// ];
+String mixTurnServerUsername = "nswd";
+String mixTurnServerCredential = "vcnswd321";
+
+int maxPublishersDefault = 9;
+
+List<RTCIceServer> iceServers = <RTCIceServer>[
+
+  RTCIceServer(urls: "stun:vc.cinteraction.com:3478", username: "", credential: ""),
+  RTCIceServer(
+      urls: "turn:vc.cinteraction.com:3478?transport=udp",
+      username: mixTurnServerUsername,
+      credential: mixTurnServerCredential),
+  RTCIceServer(
+      urls: "turn:vc.cinteraction.com:3478?transport=tcp",
+      username: mixTurnServerUsername,
+      credential: mixTurnServerCredential),
+];
 
 
 //HUAWEI
@@ -29,20 +29,20 @@ String url = "wss://cinteraction.nswebdevelopment.com:8189";
 bool withCredentials = false;
 String apiSecret = "";
 
-String mixTurnServerUsername = 'test';
-String mixTurnServerCredential = 'test123';
+// String mixTurnServerUsername = 'test';
+// String mixTurnServerCredential = 'test123';
 
-int maxPublishersDefault = 9;
+// int maxPublishersDefault = 9;
 
-List<RTCIceServer> iceServers = <RTCIceServer>[
-
-  RTCIceServer(urls: 'stun:cinteraction.nswebdevelopment.com:3478', username: "", credential: ""),
-  RTCIceServer(
-      urls: 'turn:cinteraction.nswebdevelopment.com:3478?transport=udp',
-      username: mixTurnServerUsername,
-      credential: mixTurnServerCredential),
-  RTCIceServer(
-      urls: 'turn:cinteraction.nswebdevelopment.com:3478?transport=tcp',
-      username: mixTurnServerUsername,
-      credential: mixTurnServerCredential),
-];
+// List<RTCIceServer> iceServers = <RTCIceServer>[
+//
+//   RTCIceServer(urls: 'stun:cinteraction.nswebdevelopment.com:3478', username: "", credential: ""),
+//   RTCIceServer(
+//       urls: 'turn:cinteraction.nswebdevelopment.com:3478?transport=udp',
+//       username: mixTurnServerUsername,
+//       credential: mixTurnServerCredential),
+//   RTCIceServer(
+//       urls: 'turn:cinteraction.nswebdevelopment.com:3478?transport=tcp',
+//       username: mixTurnServerUsername,
+//       credential: mixTurnServerCredential),
+// ];
