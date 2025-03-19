85140e214193730f9d734cec848cad9c0f3609c7
srdjankokot <kokotsrdjan@gmail.com>
2025-03-19 08:28:20 +0100

    set janus on huawei

--- lib/core/util/conf.dart
+++ lib/core/util/conf.dart
@@ -1,14 +1,14 @@
 import 'package:janus_client/janus_client.dart';
 
-String url = 'wss://vc.cinteraction.com:8088';
-bool withCredentials = false;
-String apiSecret = "";
-
+// String url = 'wss://vc.cinteraction.com:8088';
+// bool withCredentials = false;
+// String apiSecret = "";
+//
 String mixTurnServerUsername = "nswd";
 String mixTurnServerCredential = "vcnswd321";
-
-int maxPublishersDefault = 9;
-
+//
+// int maxPublishersDefault = 9;
+//
 List<RTCIceServer> iceServers = <RTCIceServer>[
 
   RTCIceServer(urls: "stun:vc.cinteraction.com:3478", username: "", credential: ""),
@@ -23,14 +23,14 @@ List<RTCIceServer> iceServers = <RTCIceServer>[
 ];
 
 //HUAWEI
-// String url = "wss://huawei.nswebdevelopment.com:8189";
-// bool withCredentials = false;
-// String apiSecret = "";
-//
+String url = "wss://huawei.nswebdevelopment.com:8189";
+bool withCredentials = false;
+String apiSecret = "";
+
 // String mixTurnServerUsername = 'test';
 // String mixTurnServerCredential = 'test123';
-//
-// int maxPublishersDefault = 9;
+
+int maxPublishersDefault = 9;
 
 // List<RTCIceServer> iceServers = <RTCIceServer>[
 //   // RTCIceServer(
